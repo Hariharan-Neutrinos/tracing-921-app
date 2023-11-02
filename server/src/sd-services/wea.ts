f9a2ecee816bebb8da2eb42c83ce2106491600ff
Hariharan-Neutrinos <hari.haran@neutrinos.co>
2023-11-02 17:09:28 +0530

    init

---
+++ server/src/sd-services/wea.ts
@@ -0,0 +1,246 @@
+// _neu_generated_code__dont_modify_directly_
+let instance = null;
+//CORE_REFERENCE_IMPORTS
+//append_imports_start
+
+import * as cookieParser from 'cookie-parser'; //_splitter_
+import { SDBaseService } from '../services/SDBaseService'; //_splitter_
+import { TracerService } from '../services/TracerService'; //_splitter_
+import log from '../utils/Logger'; //_splitter_
+//append_imports_end
+export class wea {
+  private sdService = new SDBaseService();
+  private tracerService = new TracerService();
+  private app;
+  private serviceBasePath: string;
+  private generatedMiddlewares: Object;
+  private serviceName: string;
+
+  private globalTimers: any;
+  private constructor(
+    app,
+    generatedeMiddlewares,
+    routeCall,
+    middlewareCall,
+    globalTimers
+  ) {
+    this.serviceName = 'wea';
+    this.app = app;
+    this.serviceBasePath = this.app.settings.base;
+    this.generatedMiddlewares = generatedeMiddlewares;
+    this.globalTimers = globalTimers;
+  }
+
+  static getInstance(
+    app?,
+    generatedeMiddlewares?,
+    routeCall?,
+    middlewareCall?,
+    globalTimers?
+  ) {
+    if (!instance) {
+      instance = new wea(
+        app,
+        generatedeMiddlewares,
+        routeCall,
+        middlewareCall,
+        globalTimers
+      );
+    }
+    instance.mountCalls(routeCall, middlewareCall);
+    return instance;
+  }
+
+  private mountCalls(routeCall, middlewareCall) {
+    if (routeCall) {
+      this.mountAllPaths();
+      this.mountAllListeners();
+    }
+    if (middlewareCall) {
+      this.generatedMiddlewares[this.serviceName] = {};
+      this.mountAllMiddlewares();
+      this.mountTimers();
+    }
+  }
+
+  async mountAllListeners() {
+    //append_listeners
+  }
+
+  async mountTimers() {
+    //appendnew_flow_wea_TimerStart
+  }
+
+  private mountAllMiddlewares() {
+    log.debug('mounting all middlewares for service :: wea');
+    //appendnew_flow_wea_MiddlewareStart
+  }
+
+  private mountAllPaths() {
+    log.debug('mounting all paths for service :: wea');
+
+    this.app['get'](
+      `${this.serviceBasePath}/ping`,
+      cookieParser(),
+      this.sdService.getMiddlesWaresBySequenceId(
+        null,
+        'pre',
+        this.generatedMiddlewares
+      ),
+
+      async (req, res, next) => {
+        let bh: any = {};
+        try {
+          bh = this.sdService.__constructDefault(
+            { local: {}, input: {} },
+            req,
+            res,
+            next
+          );
+          let parentSpanInst = null;
+          await this.sd_6ISYx2fLqfM2lG2v(bh, parentSpanInst);
+          //appendnew_next_sd_13h8m67CkhCT0XGj
+        } catch (e) {
+          return await this.errorHandler(bh, e, 'sd_13h8m67CkhCT0XGj');
+        }
+      },
+      this.sdService.getMiddlesWaresBySequenceId(
+        null,
+        'post',
+        this.generatedMiddlewares
+      )
+    );
+
+    this.app['get'](
+      `${this.serviceBasePath}/test`,
+      cookieParser(),
+      this.sdService.getMiddlesWaresBySequenceId(
+        null,
+        'pre',
+        this.generatedMiddlewares
+      ),
+
+      async (req, res, next) => {
+        let bh: any = {};
+        try {
+          bh = this.sdService.__constructDefault(
+            { local: {}, input: {} },
+            req,
+            res,
+            next
+          );
+          let parentSpanInst = null;
+          bh = await this.sd_c0iLbLFBuaUvIW8f(bh, parentSpanInst);
+          //appendnew_next_sd_Y3QhkrqCol56DLt7
+        } catch (e) {
+          return await this.errorHandler(bh, e, 'sd_Y3QhkrqCol56DLt7');
+        }
+      },
+      this.sdService.getMiddlesWaresBySequenceId(
+        null,
+        'post',
+        this.generatedMiddlewares
+      )
+    );
+    //appendnew_flow_wea_HttpIn
+  }
+  //   service flows_wea
+
+  //appendnew_flow_wea_start
+
+  async sd_6ISYx2fLqfM2lG2v(bh, parentSpanInst) {
+    try {
+      bh.web.res.status(200).send('Tracing works');
+
+      return bh;
+    } catch (e) {
+      return await this.errorHandler(bh, e, 'sd_6ISYx2fLqfM2lG2v');
+    }
+  }
+
+  async sd_c0iLbLFBuaUvIW8f(bh, parentSpanInst) {
+    try {
+      let requestOptions: any = {
+        url: 'https://alexwohlbruck.github.io/cat-facts/',
+        timeout: 30000,
+        method: 'get',
+        headers: {},
+        followRedirects: true,
+        cookies: {},
+        authType: undefined,
+        body: undefined,
+        paytoqs: false,
+        proxyConfig: undefined,
+        tlsConfig: undefined,
+        ret: 'text',
+        params: {},
+        username: undefined,
+        password: undefined,
+        token: undefined,
+        useQuerystring: false,
+      };
+      requestOptions.rejectUnauthorized = false;
+      requestOptions.tlsConfig = undefined;
+      requestOptions.proxyConfig = undefined;
+      let responseMsg: any = await this.sdService.httpRequest(
+        requestOptions.url,
+        requestOptions.timeout,
+        requestOptions.method,
+        requestOptions.headers,
+        requestOptions.followRedirects,
+        requestOptions.cookies,
+        requestOptions.authType,
+        requestOptions.body,
+        requestOptions.paytoqs,
+        requestOptions.proxyConfig,
+        requestOptions.tlsConfig,
+        requestOptions.ret,
+        requestOptions.params,
+        requestOptions.rejectUnauthorized,
+        requestOptions.username,
+        requestOptions.password,
+        requestOptions.token
+      );
+
+      bh.result = responseMsg;
+      await this.sd_Yhx3EY39AQwNqHAP(bh, parentSpanInst);
+      //appendnew_next_sd_c0iLbLFBuaUvIW8f
+      return bh;
+    } catch (e) {
+      return await this.errorHandler(bh, e, 'sd_c0iLbLFBuaUvIW8f');
+    }
+  }
+
+  async sd_Yhx3EY39AQwNqHAP(bh, parentSpanInst) {
+    try {
+      bh.web.res.status(200).send(bh.result);
+
+      return bh;
+    } catch (e) {
+      return await this.errorHandler(bh, e, 'sd_Yhx3EY39AQwNqHAP');
+    }
+  }
+
+  //appendnew_node
+
+  // error_handler_slot
+  private async errorHandler(
+    bh,
+    e,
+    src,
+    parentSpanInst?,
+    functionName?
+  ): Promise<any> {
+    console.error(e);
+    bh.error = e;
+    bh.errorSource = src;
+    bh.errorFunName = functionName;
+    this.tracerService.sendData(parentSpanInst, bh, true);
+    if (bh.web.next) {
+      bh.web.next(e);
+    } else {
+      throw e;
+    }
+  }
+  //appendnew_flow_wea_Catch
+}
